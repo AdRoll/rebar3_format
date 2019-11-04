9de5becd29c90963e148888ecd29f522f46df45d
Juan Bono <juanbono94@gmail.com>
2019-11-04 13:28:31 -0300

    add test modules (#3)
    * add test modules
    
    * add export

---
+++ test_app/src/simple_task.erl
@@ -0,0 +1,13 @@
+-module(simple_task).
+-vsn(1.0).
+-author("test author").
+-export([
+    stop/1, 
+    start/1
+    ]).
+
+-behaviour(task).
+
+stop(_Name) ->
+    ok.
+start(Spec)->task:name(Spec).
