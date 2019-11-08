72cf453440e2ae9bb5763401a370b1d8b8731878
Juan Bono <juan.bono@adroll.com>
2019-11-08 14:52:39 -0300

    fix unicode parsing and formatting

---
+++ test_app/src/unicode.erl
@@ -0,0 +1,17 @@
+
+
+-module(unicode).
+
+-export([unicode_words/0]).
+
+
+
+
+unicode_words() ->
+    Word1 = <<"Hwæt! Wē Gār‐Dena in geār‐dagum">>,
+    Word2 = <<"þēod‐cyninga þrym gefrūnon,">>,<<Word1/binary, Word2/binary>>.
+
+
+
+
+
