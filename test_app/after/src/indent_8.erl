b7d721364d899c964e7c89a8dcfac66b06d3c55d
Brujo Benavides <fernando.benavides@adroll.com>
2020-09-29 22:27:57 +0200

    Fix merge

--- test_app/after/src/indent_8.erl
+++ test_app/after/src/indent_8.erl
@@ -108,9 +108,11 @@ binary_generator() ->
 receive_after(ExpressionsThatAreReallyTooLongForALine) ->
         receive
                 clauses ->
-                        should:be(indented_using:break_indent(8))
+                        should:be(
+                                indented_using:break_indent(8))
                 after ExpressionsThatAreReallyTooLongForALine ->
-                        should:be(indented_using:break_indent(8))
+                        should:be(
+                                indented_using:break_indent(8))
         end.
 
 try_expr() ->
