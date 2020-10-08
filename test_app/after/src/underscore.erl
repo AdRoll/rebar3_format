c93f84ea80b7b135a79f28524086a7451ab32b3f
Brujo Benavides <fernando.benavides@adroll.com>
2020-10-08 18:02:43 +0200

    [Fix #168] Don't indent on underscores either (#192)
    * [Fix #168] Don't indent on underscores either
    
    * underscore -> variable

---
+++ test_app/after/src/underscore.erl
@@ -0,0 +1,20 @@
+-module(underscore).
+
+-export([x/0, x/1]).
+
+x(Be) ->
+    _ = this:code(should,
+                  not Be,
+                  indented,
+                  below,
+                  the,
+                  underscore,
+                  even_when,
+                  it,
+                  goes,
+                  over,
+                  paper).
+
+x() ->
+    SinceThisVariableIsLong =
+        this:code(should, be, indented, below, the, variable, since, it, goes, over, paper).
