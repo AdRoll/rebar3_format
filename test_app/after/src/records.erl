3ccea67cc83c6f31124807e84253d598eb4078fa
Brujo Benavides <elbrujohalcon@gmail.com>
2020-11-02 15:40:29 +0100

    [#198] Add tests

---
+++ test_app/after/src/records.erl
@@ -0,0 +1,10 @@
+-module(records).
+
+-record(record, {with_another :: #record{}, in_it}).
+
+-type record() :: #record{in_it :: 1..3}.
+
+f(A) ->
+    B = C = A#record.with_another#record.with_another#record.in_it,
+    D = A#record{with_another = #record{with_another = #record{in_it = B}, in_it = C}},
+    (normalize(D))#record.with_another#record.in_it.
