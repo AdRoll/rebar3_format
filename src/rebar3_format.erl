f805b10f9bc1b36096af82e42e50029f3d350eec
Olivier Boudeville <olivier.boudeville@esperide.com>
2022-11-11 17:43:17 +0100

    Properly notify that rebar_format shall be run as a plugin, not as an escript

--- src/rebar3_format.erl
+++ src/rebar3_format.erl
@@ -1,9 +1,14 @@
 %%% @doc Main entry point for the rebar3 format plugin
 -module(rebar3_format).
 
--export([init/1]).
+-export([init/1, main/1]).
 
 %% @private
 -spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
 init(State) ->
     rebar3_format_prv:init(State).
+
+%% @private
+-spec main(any()) -> no_return().
+main(_) ->
+    rebar_api:abort("rebar3_format shall be run as a plugin, not as an escript.", []).
