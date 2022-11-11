cacbe3340aa9d0613b0f28f1e4344592022a99e4
Olivier Boudeville <1321079+Olivier-Boudeville@users.noreply.github.com>
2022-11-12 00:08:00 +0100

    Notify escript not supported (#332)

--- src/rebar3_format.erl
+++ src/rebar3_format.erl
@@ -1,9 +1,21 @@
 %%% @doc Main entry point for the rebar3 format plugin
 -module(rebar3_format).
 
--export([init/1]).
+-elvis([{elvis_style, no_debug_call, disable}]).
+
+-hank([{unnecessary_function_arguments, [{main, 1, 1}]}]).
+
+-export([init/1, main/1]).
 
 %% @private
 -spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
 init(State) ->
     rebar3_format_prv:init(State).
+
+%% @private
+-spec main(any()) -> no_return().
+main(_) ->
+    Msg = "rebar3_format shall be run as a plugin, not as an escript.~n",
+
+    io:format(Msg),
+    exit(escript_not_supported).
